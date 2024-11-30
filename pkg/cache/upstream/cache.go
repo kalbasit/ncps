530c5db4e6eedc59d5ec180896318e3f7941043d
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-11-30 14:10:16 -0800

    wip

---
+++ pkg/cache/upstream/cache.go
@@ -0,0 +1,26 @@
+package upstream
+
+import (
+	"fmt"
+
+	"github.com/nix-community/go-nix/pkg/narinfo/signature"
+)
+
+type Cache struct {
+	Host       string
+	PublicKeys []signature.PublicKey
+}
+
+func New(host string, pubKeys []string) (Cache, error) {
+	uc := Cache{Host: host}
+
+	for _, pubKey := range pubKeys {
+		pk, err := signature.ParsePublicKey(pubKey)
+		if err != nil {
+			return uc, fmt.Errorf("error parsing the public key: %w", err)
+		}
+		uc.PublicKeys = append(uc.PublicKeys, pk)
+	}
+
+	return uc, nil
+}
