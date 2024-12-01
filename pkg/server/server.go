0eb929c040f2ffd9b4bb78cb69c8b3cce3fd362c
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-01 02:39:18 -0800

    implement the server

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"io/fs"
 	"net/http"
 	"strconv"
 	"time"
@@ -16,9 +15,10 @@ import (
 )
 
 const (
-	routeNar       = "/nar/{hash:[a-z0-9]+}.nar.{compression:*}"
-	routeNarInfo   = "/{hash:[a-z0-9]+}.narinfo"
-	routeCacheInfo = "/nix-cache-info"
+	routeNar            = "/nar/{hash:[a-z0-9]+}.nar"
+	routeNarCompression = "/nar/{hash:[a-z0-9]+}.nar.{compression:*}"
+	routeNarInfo        = "/{hash:[a-z0-9]+}.narinfo"
+	routeCacheInfo      = "/nix-cache-info"
 
 	contentLength      = "Content-Length"
 	contentType        = "Content-Type"
@@ -27,16 +27,17 @@ const (
 
 	nixCacheInfo = `StoreDir: /nix/store
 WantMassQuery: 1
-Priority: 10
-`
+Priority: 10`
 )
 
+// Server represents the main HTTP server.
 type Server struct {
 	cache  cache.Cache
 	logger log15.Logger
 	router *chi.Mux
 }
 
+// New returns a new server.
 func New(logger log15.Logger, cache cache.Cache) (Server, error) {
 	s := Server{
 		cache:  cache,
@@ -48,9 +49,11 @@ func New(logger log15.Logger, cache cache.Cache) (Server, error) {
 		return s, fmt.Errorf("error creating the router: %w", err)
 	}
 	s.router = router
+
 	return s, nil
 }
 
+// ServeHTTP implements http.Handler and turns the Server type into a handler.
 func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) { s.router.ServeHTTP(w, r) }
 
 func createRouter(s Server) (*chi.Mux, error) {
@@ -58,7 +61,6 @@ func createRouter(s Server) (*chi.Mux, error) {
 
 	router.Use(middleware.RequestID)
 	router.Use(middleware.RealIP)
-	// router.Use(middleware.Timeout(60 * time.Second))
 	router.Use(requestLogger(s.logger))
 	router.Use(middleware.Recoverer)
 
@@ -66,11 +68,12 @@ func createRouter(s Server) (*chi.Mux, error) {
 
 	router.Head(routeNarInfo, s.getNarInfo(false))
 	router.Get(routeNarInfo, s.getNarInfo(true))
-	// router.Put(routeNarInfo, s.putNarInfo())
-	//
-	// router.Head(routeNar, s.getNar(false))
-	// router.Get(routeNar, s.getNar(true))
-	// router.Put(routeNar, s.putNar())
+
+	router.Head(routeNarCompression, s.getNar(false))
+	router.Get(routeNarCompression, s.getNar(true))
+
+	router.Head(routeNar, s.getNar(false))
+	router.Get(routeNar, s.getNar(true))
 
 	return router, nil
 }
@@ -114,15 +117,51 @@ func (s Server) getNixCacheInfo(w http.ResponseWriter, r *http.Request) {
 func (s Server) getNarInfo(withBody bool) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		hash := chi.URLParam(r, "hash")
-		size, f, err := s.cache.GetNarInfo(hash)
+		narInfo, err := s.cache.GetNarInfo(r.Context(), hash)
 		if err != nil {
-			if errors.Is(err, fs.ErrNotExist) {
+			if errors.Is(err, cache.ErrNotFound) {
 				w.WriteHeader(http.StatusNotFound)
 				w.Write([]byte(http.StatusText(http.StatusNotFound)))
 				return
 			}
 
 			s.logger.Error("error fetching the narinfo", "hash", hash, "error", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(http.StatusText(http.StatusInternalServerError)))
+			return
+		}
+
+		narInfoBytes := []byte(narInfo.String())
+
+		h := w.Header()
+		h.Set(contentType, contentTypeNarInfo)
+		h.Set(contentLength, strconv.Itoa(len(narInfoBytes)))
+
+		if !withBody {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		if _, err := w.Write(narInfoBytes); err != nil {
+			s.logger.Error("error writing the narinfo to the response", "error", err)
+		}
+	}
+}
+
+func (s Server) getNar(withBody bool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		hash := chi.URLParam(r, "hash")
+		compression := chi.URLParam(r, "compression")
+
+		size, reader, err := s.cache.GetNar(r.Context(), hash, compression)
+		if err != nil {
+			if errors.Is(err, cache.ErrNotFound) {
+				w.WriteHeader(http.StatusNotFound)
+				w.Write([]byte(http.StatusText(http.StatusNotFound)))
+				return
+			}
+
+			s.logger.Error("error fetching the nar", "hash", hash, "compression", compression, "error", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(http.StatusText(http.StatusInternalServerError)))
 			return
@@ -137,7 +176,7 @@ func (s Server) getNarInfo(withBody bool) http.HandlerFunc {
 			return
 		}
 
-		written, err := io.CopyN(w, f, size)
+		written, err := io.Copy(w, reader)
 		if written != size {
 			s.logger.Error("Bytes copied does not match object size", "expected", size, "written", written)
 		}
---
+++ pkg/server/server_test.go
@@ -0,0 +1,209 @@
+package server_test
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"testing"
+
+	"github.com/inconshreveable/log15/v3"
+	"github.com/kalbasit/ncps/pkg/cache"
+	"github.com/kalbasit/ncps/pkg/cache/upstream"
+	"github.com/kalbasit/ncps/pkg/server"
+)
+
+//nolint:gochecknoglobals
+var logger = log15.New()
+
+//nolint:gochecknoinits
+func init() {
+	logger.SetHandler(log15.DiscardHandler())
+}
+
+const (
+	nixStoreInfo = `StoreDir: /nix/store
+WantMassQuery: 1
+Priority: 40`
+
+	narInfoHash = "7bn85d74qa0127p85rrswfyghxsqmcf7"
+
+	//nolint:lll
+	narInfoText = `StorePath: /nix/store/7bn85d74qa0127p85rrswfyghxsqmcf7-iputils-20210722
+URL: nar/136jk8xlxqzqd16d00dpnnpgffmycwm66zgky6397x75yg7ylz00.nar.xz
+Compression: xz
+FileHash: sha256:136jk8xlxqzqd16d00dpnnpgffmycwm66zgky6397x75yg7ylz00
+FileSize: 132228
+NarHash: sha256:1rzb80kz42wy067pp160rridw41dnc09d2a3cqj2wdg6ylklhxkh
+NarSize: 534160
+References: 7bn85d74qa0127p85rrswfyghxsqmcf7-iputils-20210722 892cxk44qxzzlw9h90a781zpy1j7gmmn-libidn2-2.3.2 l25bc19is0s27929kxkfhgdzhc7x9g5m-libcap-2.49-lib rir9pf0kz1mb84x5bd3yr0fx415yy423-glibc-2.33-123
+Deriver: 9fs4vq4gdsb8r9ywawb5f6zl40ycp1bh-iputils-20210722.drv
+Sig: cache.nixos.org-1:WzhkqDdkgPz2qU/0QyEA6wUIm7EMR5MY8nTb5jAmmoh5b80ACIp/+Zpgi5t1KvmO8uG8GVrkPejCxbyQ2gNXDQ==
+`
+
+	narHash = "136jk8xlxqzqd16d00dpnnpgffmycwm66zgky6397x75yg7ylz00"
+
+	narText = "Hello, World" // fake nar for above nar info
+)
+
+//nolint:paralleltest
+func TestServeHTTP(t *testing.T) {
+	us := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/nix-cache-info" {
+			if _, err := w.Write([]byte(nixStoreInfo)); err != nil {
+				t.Fatalf("expected no error got: %s", err)
+			}
+
+			return
+		}
+
+		if r.URL.Path == "/"+narInfoHash+".narinfo" {
+			if _, err := w.Write([]byte(narInfoText)); err != nil {
+				t.Fatalf("expected no error got: %s", err)
+			}
+
+			return
+		}
+
+		if r.URL.Path == "/nar/"+narHash+".nar" {
+			if _, err := w.Write([]byte(narText)); err != nil {
+				t.Fatalf("expected no error got: %s", err)
+			}
+
+			return
+		}
+
+		if r.URL.Path == "/nar/"+narHash+".nar.xz" {
+			if _, err := w.Write([]byte(narText + "xz")); err != nil {
+				t.Fatalf("expected no error got: %s", err)
+			}
+
+			return
+		}
+
+		w.WriteHeader(http.StatusNotFound)
+	}))
+	defer us.Close()
+
+	uu, err := url.Parse(us.URL)
+	if err != nil {
+		t.Fatalf("error not expected, got %s", err)
+	}
+
+	dir, err := os.MkdirTemp("", "cache-path-")
+	if err != nil {
+		t.Fatalf("expected no error, got: %q", err)
+	}
+	defer os.RemoveAll(dir) // clean up
+
+	uc, err := upstream.New(logger, uu.Host, []string{"cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="})
+	if err != nil {
+		t.Fatalf("expected no error, got %s", err)
+	}
+
+	c, err := cache.New(logger, "cache.example.com", dir, []upstream.Cache{uc})
+	if err != nil {
+		t.Fatalf("expected no error, got %q", err)
+	}
+
+	s, err := server.New(logger, c)
+	if err != nil {
+		t.Fatalf("expected no error, got %q", err)
+	}
+
+	t.Run("narinfo", func(t *testing.T) {
+		t.Run("narfile does not exist upstream", func(t *testing.T) {
+			r := httptest.NewRequest("GET", "/doesnotexist.narinfo", nil)
+			w := httptest.NewRecorder()
+
+			s.ServeHTTP(w, r)
+
+			if want, got := http.StatusNotFound, w.Code; want != got {
+				t.Errorf("want %d got %d", want, got)
+			}
+		})
+
+		t.Run("narfile exists upstream", func(t *testing.T) {
+			r := httptest.NewRequest("GET", "/"+narInfoHash+".narinfo", nil)
+			w := httptest.NewRecorder()
+
+			s.ServeHTTP(w, r)
+
+			if want, got := http.StatusOK, w.Code; want != got {
+				t.Errorf("want %d got %d", want, got)
+			}
+
+			resp := w.Result()
+			defer resp.Body.Close()
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("expected no error got %s", err)
+			}
+
+			if want, got := narInfoText, string(body); want != got {
+				t.Errorf("want %q got %q", want, got)
+			}
+		})
+	})
+
+	t.Run("nar", func(t *testing.T) {
+		t.Run("nar does not exist upstream", func(t *testing.T) {
+			r := httptest.NewRequest("GET", "/nar/doesnotexist.nar", nil)
+			w := httptest.NewRecorder()
+
+			s.ServeHTTP(w, r)
+
+			if want, got := http.StatusNotFound, w.Code; want != got {
+				t.Errorf("want %d got %d", want, got)
+			}
+		})
+
+		t.Run("nar exists upstream without compression", func(t *testing.T) {
+			r := httptest.NewRequest("GET", "/nar/"+narHash+".nar", nil)
+			w := httptest.NewRecorder()
+
+			s.ServeHTTP(w, r)
+
+			if want, got := http.StatusOK, w.Code; want != got {
+				t.Errorf("want %d got %d", want, got)
+			}
+
+			resp := w.Result()
+			defer resp.Body.Close()
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("expected no error got %s", err)
+			}
+
+			if want, got := narText, string(body); want != got {
+				t.Errorf("want %q got %q", want, got)
+			}
+		})
+
+		t.Run("nar exists upstream with compression", func(t *testing.T) {
+			r := httptest.NewRequest("GET", "/nar/"+narHash+".nar.xz", nil)
+			w := httptest.NewRecorder()
+
+			s.ServeHTTP(w, r)
+
+			if want, got := http.StatusOK, w.Code; want != got {
+				t.Errorf("want %d got %d", want, got)
+			}
+
+			resp := w.Result()
+			defer resp.Body.Close()
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("expected no error got %s", err)
+			}
+
+			if want, got := narText+"xz", string(body); want != got {
+				t.Errorf("want %q got %q", want, got)
+			}
+		})
+	})
+}
