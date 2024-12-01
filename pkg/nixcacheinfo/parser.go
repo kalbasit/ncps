07a65a9cce2016d8ed5e8b8c8006dee5297177e9
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-11-30 18:55:46 -0800

    fix all lint issues

--- pkg/nixcacheinfo/parser.go
+++ pkg/nixcacheinfo/parser.go
@@ -12,10 +12,8 @@ import (
 // NOTE: Inspired from:
 // https://github.com/nix-community/go-nix/blob/0327d78224c2de28edd957d2ef4240711217d7fe/pkg/narinfo/parser.go#L1
 
-var (
-	// ErrUnknownKey is returned if the nix-cache-info contains a key that is not known
-	ErrUnknownKey = errors.New("error the key is not known")
-)
+// ErrUnknownKey is returned if the nix-cache-info contains a key that is not known.
+var ErrUnknownKey = errors.New("error the key is not known")
 
 // NixCacheInfo represents the nix cache info parsed.
 type NixCacheInfo struct {
--- pkg/nixcacheinfo/splitonce.go
+++ pkg/nixcacheinfo/splitonce.go
@@ -1,6 +1,7 @@
 package nixcacheinfo
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 )
@@ -8,15 +9,23 @@ import (
 // NOTE: Copied from:
 // https://github.com/nix-community/go-nix/blob/0327d78224c2de28edd957d2ef4240711217d7fe/pkg/narinfo/parser.go#L109-L120
 
+var (
+	// ErrSplitOnceFoundMultipleSeparators is returned if splitOnce encountered multiple separators in the string.
+	ErrSplitOnceFoundMultipleSeparators = errors.New("found multiple separators in the string")
+
+	// ErrSplitOnceFoundNoSeparators is returned if splitOnce encountered multiple separators in the string.
+	ErrSplitOnceFoundNoSeparators = errors.New("found no separators in the string")
+)
+
 // splitOnce - Split a string and make sure it's only splittable once.
 func splitOnce(s string, sep string) (string, string, error) {
 	idx := strings.Index(s, sep)
 	if idx == -1 {
-		return "", "", fmt.Errorf("unable to find separator '%s' in %v", sep, s)
+		return "", "", fmt.Errorf("%w: separator=%q string=%q", ErrSplitOnceFoundNoSeparators, sep, s)
 	}
 
 	if strings.Contains(s[idx+1:], sep) {
-		return "", "", fmt.Errorf("found separator '%s' twice or more in %v", sep, s)
+		return "", "", fmt.Errorf("%w: separator=%q string=%q", ErrSplitOnceFoundMultipleSeparators, sep, s)
 	}
 
 	return s[0:idx], s[idx+len(sep):], nil
