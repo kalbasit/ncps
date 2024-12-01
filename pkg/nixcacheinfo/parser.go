b940fe675de518591f80034dabbdfe7f676fee30
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-11-30 18:49:34 -0800

    fix lint

--- pkg/nixcacheinfo/parser.go
+++ pkg/nixcacheinfo/parser.go
@@ -2,14 +2,20 @@ package nixcacheinfo
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"strconv"
 	"strings"
 )
 
-// NOTE: Copied from:
+// NOTE: Inspired from:
 // https://github.com/nix-community/go-nix/blob/0327d78224c2de28edd957d2ef4240711217d7fe/pkg/narinfo/parser.go#L1
+
+var (
+	// ErrUnknownKey is returned if the nix-cache-info contains a key that is not known
+	ErrUnknownKey = errors.New("error the key is not known")
+)
 
 // NixCacheInfo represents the nix cache info parsed.
 type NixCacheInfo struct {
@@ -18,17 +24,17 @@ type NixCacheInfo struct {
 	Priority      uint64
 }
 
+// ParseString parser the NixCacheInfo from a string.
 func ParseString(nci string) (NixCacheInfo, error) {
 	return Parse(strings.NewReader(nci))
 }
 
+// Parse parses the NixCacheInfo from an io.Reader.
 func Parse(r io.Reader) (NixCacheInfo, error) {
 	nci := NixCacheInfo{}
 	scanner := bufio.NewScanner(r)
 
 	for scanner.Scan() {
-		var err error
-
 		line := scanner.Text()
 		// skip empty lines (like, an empty line at EOF)
 		if line == "" {
@@ -46,24 +52,20 @@ func Parse(r io.Reader) (NixCacheInfo, error) {
 		case "WantMassQuery":
 			nci.WantMassQuery, err = strconv.ParseUint(v, 10, 0)
 			if err != nil {
-				return nci, err
+				return nci, fmt.Errorf("error parsing %q as uint64: %w", v, err)
 			}
 		case "Priority":
 			nci.Priority, err = strconv.ParseUint(v, 10, 0)
 			if err != nil {
-				return nci, err
+				return nci, fmt.Errorf("error parsing %q as uint64: %w", v, err)
 			}
 		default:
-			return nci, fmt.Errorf("unknown key %v", k)
-		}
-
-		if err != nil {
-			return nci, fmt.Errorf("unable to parse line %v", line)
+			return nci, fmt.Errorf("%w: %q", ErrUnknownKey, k)
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
-		return nci, err
+		return nci, fmt.Errorf("scanner error: %w", err)
 	}
 
 	return nci, nil
