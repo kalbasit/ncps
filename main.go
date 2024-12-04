2850be8f01117b0bc92356313838c6e5ee7fbe98
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-04 15:53:47 -0800

    Move main to the root directory (#24)
    Since the project is using a CLI, we can add sub-commands easily so we
    don't really need a way to add other `main` packages.

--- main.go
+++ main.go
@@ -6,18 +6,9 @@ import (
 	"os"
 
 	"github.com/inconshreveable/log15/v3"
+	"github.com/kalbasit/ncps/cmd"
 	"github.com/mattn/go-colorable"
-	"github.com/urfave/cli/v3"
 	"golang.org/x/term"
-)
-
-var (
-	// Version defines the version of the binary, and is meant to be set with ldflags at build time.
-	//nolint:gochecknoglobals
-	Version = "dev"
-
-	//nolint:gochecknoglobals
-	logger log15.Logger
 )
 
 func main() {
@@ -25,23 +16,16 @@ func main() {
 }
 
 func realMain() int {
-	logger = log15.New()
+	logger := log15.New()
 	if term.IsTerminal(int(os.Stdout.Fd())) {
 		logger.SetHandler(log15.StreamHandler(colorable.NewColorableStdout(), log15.TerminalFormat()))
 	} else {
 		logger.SetHandler(log15.StreamHandler(os.Stdout, log15.JsonFormat()))
 	}
 
-	cmd := &cli.Command{
-		Name:    "ncps",
-		Usage:   "Nix Binary Cache Proxy Service",
-		Version: Version,
-		Commands: []*cli.Command{
-			serveCommand,
-		},
-	}
+	c := cmd.New(logger)
 
-	if err := cmd.Run(context.Background(), os.Args); err != nil {
+	if err := c.Run(context.Background(), os.Args); err != nil {
 		log.Printf("error running the application: %s", err)
 
 		return 1
