6f46a79a5c99315351adc001307f193879358a91
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-04 19:18:16 -0800

    add funcs and their tests

--- pkg/database/sqlite.go
+++ pkg/database/sqlite.go
@@ -3,6 +3,7 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	"github.com/inconshreveable/log15/v3"
 
@@ -39,12 +40,41 @@ const (
 	`
 )
 
-type DB struct {
-	*sql.DB
+type (
+	// DB is the main database wrapping *sql.DB and have functions that can
+	// operate on nar and narinfos.
+	DB struct {
+		*sql.DB
 
-	logger log15.Logger
-}
+		logger log15.Logger
+	}
 
+	// NarInfoModel represents a narinfo record in the database; This is not the
+	// same as narinfo.NarInfo!
+	NarInfoModel struct {
+		ID   int64
+		Hash string
+
+		CreatedAt      time.Time
+		UpdatedAt      *time.Time
+		LastAccessedAt time.Time
+	}
+
+	// NarModel represents a nar record in the database.
+	NarModel struct {
+		ID          int64
+		NarInfoID   int64
+		Hash        string
+		Compression string
+		FileSize    int64
+
+		CreatedAt      time.Time
+		UpdatedAt      *time.Time
+		LastAccessedAt time.Time
+	}
+)
+
+// Open opens a sqlite3 database, and creates it if necessary.
 func Open(logger log15.Logger, dbpath string) (*DB, error) {
 	sdb, err := sql.Open("sqlite3", dbpath)
 	if err != nil {
@@ -54,6 +84,21 @@ func Open(logger log15.Logger, dbpath string) (*DB, error) {
 	db := &DB{DB: sdb, logger: logger.New("dbpath", dbpath)}
 
 	return db, db.createTables()
+}
+
+// InsertNarInfoRecord creates a new narinfo record in the database.
+func (db *DB) InsertNarInfoRecord(tx *sql.Tx, hash string) error {
+	stmt, err := tx.Prepare("INSERT into narinfos(hash) VALUES (?)")
+	if err != nil {
+		return fmt.Errorf("error preparing a statement: %w", err)
+	}
+	defer stmt.Close()
+
+	if _, err := stmt.Exec(hash); err != nil {
+		return fmt.Errorf("error executing the statement: %w", err)
+	}
+
+	return nil
 }
 
 func (db *DB) createTables() error {
--- pkg/database/sqlite_test.go
+++ pkg/database/sqlite_test.go
@@ -3,10 +3,13 @@ package database_test
 import (
 	"os"
 	"path/filepath"
+	"reflect"
 	"testing"
+	"time"
 
 	"github.com/inconshreveable/log15/v3"
 	"github.com/kalbasit/ncps/pkg/database"
+	"github.com/kalbasit/ncps/pkg/helper"
 )
 
 //nolint:gochecknoglobals
@@ -77,7 +80,7 @@ func TestOpen(t *testing.T) {
 		t.Run("database has the nars table", func(t *testing.T) {
 			rows, err := db.Query("SELECT name FROM sqlite_master WHERE type=? AND name=?", "table", "nars")
 			if err != nil {
-				t.Fatalf("error inserting a narinfo: %s", err)
+				t.Fatalf("error querying sqlite_master: %s", err)
 			}
 
 			defer rows.Close()
@@ -101,5 +104,83 @@ func TestOpen(t *testing.T) {
 				t.Errorf("want %q got %q", want, got)
 			}
 		})
+	})
+}
+
+func TestInsertNarInfoRecord(t *testing.T) {
+	dir, err := os.MkdirTemp("", "database-path-")
+	if err != nil {
+		t.Fatalf("expected no error, got: %q", err)
+	}
+	defer os.RemoveAll(dir) // clean up
+
+	dbpath := filepath.Join(dir, "db.sqlite")
+
+	db, err := database.Open(logger, dbpath)
+	if err != nil {
+		t.Fatalf("expected no error but got: %s", err)
+	}
+
+	t.Run("inserting one record", func(t *testing.T) {
+		hash, err := helper.RandString(32, nil)
+		if err != nil {
+			t.Fatalf("expected no error but got: %s", err)
+		}
+
+		tx, err := db.Begin()
+		if err != nil {
+			t.Fatalf("expected no error but got: %s", err)
+		}
+
+		if err := db.InsertNarInfoRecord(tx, hash); err != nil {
+			t.Fatalf("expected no error got: %s", err)
+		}
+
+		if err := tx.Commit(); err != nil {
+			t.Fatalf("expected no error got: %s", err)
+		}
+
+		rows, err := db.Query("SELECT id, hash, created_at, updated_at, last_accessed_at FROM narinfos")
+		if err != nil {
+			t.Fatalf("error selecting narinfos: %s", err)
+		}
+
+		defer rows.Close()
+
+		nims := make([]database.NarInfoModel, 0)
+		for rows.Next() {
+			var nim database.NarInfoModel
+
+			if err := rows.Scan(&nim.ID, &nim.Hash, &nim.CreatedAt, &nim.UpdatedAt, &nim.LastAccessedAt); err != nil {
+				t.Fatalf("expected no error got: %s", err)
+			}
+
+			nims = append(nims, nim)
+		}
+
+		if want, got := 1, len(nims); want != got {
+			t.Fatalf("want %d got %d", want, got)
+		}
+
+		if want, got := int64(1), nims[0].ID; want != got {
+			t.Errorf("want %d got %d", want, got)
+		}
+
+		if want, got := hash, nims[0].Hash; want != got {
+			t.Errorf("want %s got %s", want, got)
+		}
+
+		old := time.Now().Sub(nims[0].CreatedAt)
+		if old > time.Duration(3*time.Second) {
+			t.Errorf("expected the nim to have a created at less than 3s got: %s", old)
+		}
+
+		if nims[0].UpdatedAt != nil {
+			t.Errorf("expected no updated_at field, found: %s", nims[0].UpdatedAt)
+		}
+
+		if want, got := nims[0].CreatedAt, nims[0].LastAccessedAt; !reflect.DeepEqual(want, got) {
+			t.Errorf("want %s got %s", want, got)
+		}
 	})
 }
