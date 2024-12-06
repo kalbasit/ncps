f95c7635547e56c9c63cce8ad7132b731463481b
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-05 23:36:43 -0800

    wip

--- testdata/nar2.go
+++ testdata/nar2.go
@@ -1,10 +1,8 @@
 package testdata
 
-const (
-	NarInfoHash2 = "a6hiaxlxf7arxsf59f4rzs7b337z6a11"
-
-	//nolint:lll
-	NarInfoText2 = `StorePath: /nix/store/a6hiaxlxf7arxsf59f4rzs7b337z6a11-brave-1.73.91
+var Nar2 = Entry{
+	NarInfoHash: "a6hiaxlxf7arxsf59f4rzs7b337z6a11",
+	NarInfoText: `StorePath: /nix/store/a6hiaxlxf7arxsf59f4rzs7b337z6a11-brave-1.73.91
 URL: nar/1mb5fxh7nzbx1b2q40bgzwjnjh8xqfap9mfnfqxlvvgvdyv8xwps.nar
 Compression: xz
 FileHash: sha256:1mb5fxh7nzbx1b2q40bgzwjnjh8xqfap9mfnfqxlvvgvdyv8xwps
@@ -13,9 +11,8 @@ NarHash: sha256:1q95nilzb512ckljzj7h8bsdnj4qhpvyddp74bhsf8sxb8vjmpsj
 NarSize: 388038816
 References: 08mdbxgs2m7fz3hx2xrqrbmhldxagpf8-pango-1.54.0-bin 0irlcqx2n3qm6b1pc9rsd2i8qpvcccaj-bash-5.2p37 2542sr1ch7ypz52w423vyn0fhn934rj7-dbus-1.14.10 3c603pkfh9fq13lrm6rdqynb7xxwazv7-libXrandr-1.5.4 3ma7c4syxds2wrp03ax3b7p9fdsh1lwg-nss-3.101.2 4qiqy2z0facj37682n9x0mz0isd9k3qb-libXScrnSaver-1.2.4 4zjxqvvnnisn8nqmibgy40rvm087v85n-cups-2.4.11-lib 566jyp236h118ad005mxqs7sb9ivv4yj-libXrender-0.9.11 5lq743a2jp3v4kgr80y0sw8jr70pl6gj-libXi-1.8.2 7c88jgbh10zvlv91bmhyc40h8l9b3wf5-librsvg-2.58.3 84839y8j0d4l4d3q048v00h20jdk9lh0-wayland-1.23.1 946m0cmd00hcw2s6slzp5qc01krgrzks-dbus-1.14.10-lib 9i9wrwdi1hkcklzya0yhci0d0vkyl79k-alsa-lib-1.2.12 a6hiaxlxf7arxsf59f4rzs7b337z6a11-brave-1.73.91 apmrnyl4b6fj9czqkb8vwpf6cvfvs0ba-glib-2.82.1-bin arrgysrp5ks44qidsf8byjj11pnkaf65-cairo-1.18.2 b4ph9mwdv628db2vqhhhp3azalq6cwbq-libxcb-1.17.0 c1lszwlw825y6vr4qik6dqv2lszqk08r-libdrm-2.4.123-bin ckfsxyla4krxn4zdlmxkp6lk8nsscr5v-libXcursor-1.2.2 dcj4w0a7472g86yf5wqz5vfbd809s4cj-snappy-1.2.1 gsdkqm4qfbzr22m20cns3iilxd55ridd-freetype-2.13.3 hm2rpszabwpvs28jwkaz7pg0dqslm4bd-util-linux-minimal-2.39.4-lib hpd1dss7lmrywlr37vncyzbhzp6rgri5-libXext-1.3.6 i0d0ws5xwix1dvmwp8w1fr4kz61k11x4-libxkbcommon-1.7.0 i66c6m4680fdgfj5fmaclma1bbl9y8i6-systemd-minimal-libs-256.7 ibr7wf806ns8517zggai9yygz07kp85a-libdrm-2.4.123 idrnfiw9r87giqiqwr36xkch4dsr0rdz-pango-1.54.0 j6wpl172pz2323fia7cbjx9lznsc47ri-at-spi2-core-2.54.0 jbck0ahim6cbjjzl1wmrch5inhbhkkra-gtk4-4.16.3 jq1ydifw3ip1vx94dl8w8bgvr07l6s3x-glib-2.82.1 jx4rwrk7cg5v64ygbpyi1v3gfp8c7k9h-fontconfig-2.15.0-bin k48bha2fjqzarg52picsdfwlqx75aqbb-coreutils-9.5 lafwzwzrj53prpzgmmq0aq5spal91q7s-libXdamage-1.1.6 lcq3ibmsb6c2jgqp3yfi1yp773x5wz19-mesa-24.2.6 ldqjvk81fdzygd85505lzc781vssfpdy-libXtst-1.2.5 m761lp9x14i33hb3q7a8wm52rsnk6ab1-expat-2.6.4 mafw0r1djqvl36by8qhz02kmaggh24v8-gdk-pixbuf-2.42.12 mhp5jkwqgyz0s7klvpl23x3axgvn6msm-pipewire-1.2.6 ms2562w6dkqa6xzpaxi4n2ib9kbb3gya-libXcomposite-0.4.6 p5nz6gdblng41fiqqb7z88l119dc9v56-gsettings-desktop-schemas-47.1 pacbfvpzqz2mksby36awvbcn051zcji3-glibc-2.40-36 pg3xw94i23l4dkrxcp7l11a2iydxc1za-dconf-0.40.0-lib q8y8kgidr4mg0q7zr2h1ddpd7ijy3wna-libpulseaudio-17.0 qrj5f1mb08k5jg7y9ikzb1njli0gvmxz-libX11-1.8.10 rdjcf9pxdlcd6ncsiqm97gq4ard6c91f-nspr-4.36 rh6zrnvbzjsfy444w5w2k5sz72cl6f4z-libva-2.22.0 rmfav7fq6rmpv3d785v462shsy1njq6f-fontconfig-2.15.0-lib sawcd2sh4p7kdggj00i67wa59swaid5b-util-linux-minimal-2.39.4-bin vbw9c3pvwli5iidik8dddgz423wr1h18-gtk+3-3.24.43 w3clwb8c7c8vc7ls163p5pjspccgcrhj-cups-2.4.11 w9g781rqrc12870vji13hycwh47lwsgd-qtbase-6.8.0 wfqs8h2mf4ib7f1phpsgv0b20xny9zzl-libXfixes-6.0.1 wjnznkh0x4744mcvhlnr4qnnig15287c-xdg-utils-1.2.1 xghyamhbjj0rfavxj76l52mv7zr7dfiw-krb5-1.21.3-lib xk9b2k475ab7q9xmcqcfb1xcyh9arswj-krb5-1.21.3 y6bcc1vzg315zzzpir608zkhnmvp49kc-zlib-1.3.1 ymhcg6x5jrw3hx8ik1cji6awiybgp9f7-libglvnd-1.7.0 zm54lx2l4jsc0dw1cnmyg8ilcn07v6zb-libxshmfence-1.3.2
 Deriver: n85jhy5b8c9l1a4d9pnf3dv8bfvmyzhb-brave-1.73.91.drv
-Sig: cache.nixos.org-1:QVc2ad4OI/2G5gVyFsoWYr+AECFDXjwF6o1HOfSbMJhPp5l4iD38WgJhf2w/3kZ/B5ftq6ytDijcAWCSDItJDQ==`
+Sig: cache.nixos.org-1:QVc2ad4OI/2G5gVyFsoWYr+AECFDXjwF6o1HOfSbMJhPp5l4iD38WgJhf2w/3kZ/B5ftq6ytDijcAWCSDItJDQ==`,
 
-	NarHash2 = "1mb5fxh7nzbx1b2q40bgzwjnjh8xqfap9mfnfqxlvvgvdyv8xwps"
-
-	NarText2 = "Wave, World" // fake nar for above nar info
-)
+	NarHash: "1mb5fxh7nzbx1b2q40bgzwjnjh8xqfap9mfnfqxlvvgvdyv8xwps",
+	NarText: "Wave, World",
+}
