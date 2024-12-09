3a0bfa0c7c58cf17f818e7069bd7f3ff049e12b9
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-08 21:38:13 -0800

    pkg/helper: add ParseSize a helper to parse size with units (#59)
    For LRU functionality, I need a way to parse "9G" or "10T" etc..
    
    ref #21

---
+++ pkg/helper/size_units.go
@@ -0,0 +1,35 @@
+package helper
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// ErrInvalidSizeSuffix is returned if the suffix is not valid.
+var ErrInvalidSizeSuffix = errors.New("invalid size suffix")
+
+// ParseSize parses size with units and returns the same size in bytes.
+func ParseSize(str string) (uint64, error) {
+	num, err := strconv.ParseUint(str[:len(str)-1], 10, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	suffix := strings.ToUpper(str[len(str)-1:])
+	switch suffix {
+	case "B":
+		return num, nil
+	case "K":
+		return num * 1024, nil
+	case "M":
+		return num * 1024 * 1024, nil
+	case "G":
+		return num * 1024 * 1024 * 1024, nil
+	case "T":
+		return num * 1024 * 1024 * 1024 * 1024, nil
+	default:
+		return 0, fmt.Errorf("error parsing the unit for %q: %w", str, ErrInvalidSizeSuffix)
+	}
+}
