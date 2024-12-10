acff61e9c4975c16ade3cc182a7268312ee73c9b
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-09 21:56:38 -0800

    convert schema into migrations

---
+++ db/migrations/20241210054829_create-nars-table.sql
@@ -0,0 +1,23 @@
+-- migrate:up
+CREATE TABLE nars (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  narinfo_id INTEGER NOT NULL REFERENCES narinfos(id),
+  hash TEXT NOT NULL UNIQUE,
+  compression TEXT NOT NULL DEFAULT '',
+  file_size INTEGER NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP,
+  last_accessed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE UNIQUE INDEX idx_nars_id ON nars (id);
+CREATE UNIQUE INDEX idx_nars_hash ON nars (hash);
+CREATE INDEX idx_nars_narinfo_id ON nars (narinfo_id);
+CREATE INDEX idx_nars_last_accessed_at ON nars (last_accessed_at);
+
+-- migrate:down
+DROP INDEX idx_nars_id;
+DROP INDEX idx_nars_hash;
+DROP INDEX idx_nars_narinfo_id;
+DROP INDEX idx_nars_last_accessed_at;
+DROP TABLE nars;
