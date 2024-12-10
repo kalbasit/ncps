c30beb05e42f629bad18ddb28624a3f97eac2f91
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-09 19:38:57 -0800

    totally failing now

--- testdata/nars_test.go
+++ testdata/nars_test.go
@@ -1,7 +1,6 @@
 package testdata
 
 import (
-	"fmt"
 	"strings"
 	"testing"
 
@@ -14,20 +13,9 @@ func TestNarsValid(t *testing.T) {
 	t.Parallel()
 
 	for i, nar := range Entries {
-		var ni *narinfo.NarInfo
-
-		t.Run(fmt.Sprintf("can parse the narinfo for nar%d", i), func(t *testing.T) {
-			var err error
-			ni, err = narinfo.Parse(strings.NewReader(nar.NarInfoText))
-			require.NoError(t, err)
-		})
-
-		t.Run(fmt.Sprintf("narinfo for nar%d is valid", i), func(t *testing.T) {
-			require.NoError(t, ni.Check())
-		})
-
-		t.Run(fmt.Sprintf("the nar is of the same size as it should be for nar%d", i), func(t *testing.T) {
-			assert.EqualValues(t, ni.FileSize, len(nar.NarText))
-		})
+		ni, err := narinfo.Parse(strings.NewReader(nar.NarInfoText))
+		require.NoError(t, err)
+		require.NoError(t, ni.Check())
+		assert.EqualValues(t, ni.FileSize, len(nar.NarText))
 	}
 }
