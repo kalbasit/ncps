493b3cfd94a870b894e36b049b654aa8d68e4063
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-12 08:43:32 -0800

    devshells: Add Delve for debugging (#77)
    Add Delve to the devshells; Disable nix hardening for fortify otherwize
    delve does not start. See:
    https://wiki.nixos.org/wiki/Go#Using_cgo_on_NixOS

--- nix/devshells/flake-module.nix
+++ nix/devshells/flake-module.nix
@@ -5,6 +5,7 @@
       devShells.default = pkgs.mkShell {
         buildInputs = [
           pkgs.dbmate
+          pkgs.delve
           pkgs.go
           pkgs.golangci-lint
           pkgs.sqlc
@@ -12,6 +13,9 @@
 
         _GO_VERSION = "${pkgs.go.version}";
         _DBMATE_VERSION = "${pkgs.dbmate.version}";
+
+        # Disable hardening for fortify otherwize it's not possible to use Delve.
+        hardeningDisable = [ "fortify" ];
 
         shellHook = ''
           ${config.pre-commit.installationScript}
