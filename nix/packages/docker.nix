89302bdb2a3273f05e63d30efebeb28233f8762b
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-12 11:25:29 -0800

    improve docker pkg

--- nix/packages/docker.nix
+++ nix/packages/docker.nix
@@ -6,15 +6,20 @@
       ...
     }:
     {
-      packages.docker = pkgs.dockerTools.buildImage {
-        name = "ncps";
+      packages.docker = pkgs.dockerTools.buildLayeredImage {
+        name = "kalbasit/ncps";
         tag = "latest";
-        copyToRoot = [
-          config.packages.ncps
+        contents = [
           pkgs.dbmate
+
+          config.packages.ncps
         ];
         config = {
           Cmd = [ "/bin/ncps" ];
+          Env = [
+            "DBMATE_MIGRATIONS_DIR=/share/ncps/db/migrations"
+            "DBMATE_NO_DUMP_SCHEMA=true"
+          ];
           ExposedPorts = {
             "8501/tcp" = { };
           };
