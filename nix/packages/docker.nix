bf3e9cb6d5b52d96b612dc652b83b4da9af253e3
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-12 08:00:29 -0800

    flake: add a docker package (#90)
    It's not used yet, but I intend to eventually make use of it instead of
    using a Dockerfile.

---
+++ nix/packages/docker.nix
@@ -0,0 +1,24 @@
+{
+  perSystem =
+    {
+      config,
+      pkgs,
+      ...
+    }:
+    {
+      packages.docker = pkgs.dockerTools.buildImage {
+        name = "ncps";
+        tag = "latest";
+        copyToRoot = [
+          config.packages.ncps
+          pkgs.dbmate
+        ];
+        config = {
+          Cmd = [ "/bin/ncps" ];
+          ExposedPorts = {
+            "8501/tcp" = { };
+          };
+        };
+      };
+    };
+}
