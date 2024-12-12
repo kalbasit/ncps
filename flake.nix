7606e11e0acb5e71259564c3d695343da7874921
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-12 01:04:28 -0800

    wip

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "ncps - Nix binary cache proxy service";
+
+  inputs = {
+
+    flake-parts = {
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+      url = "github:hercules-ci/flake-parts";
+    };
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+    };
+}
