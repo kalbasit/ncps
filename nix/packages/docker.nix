966f838cce9b8f27444ce348506114ceb0d7fb23
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-12 14:24:20 -0800

    add docker image pusher

--- nix/packages/docker.nix
+++ nix/packages/docker.nix
@@ -8,7 +8,6 @@
     {
       packages.docker = pkgs.dockerTools.buildLayeredImage {
         name = "kalbasit/ncps";
-        tag = "latest";
         contents = [
           pkgs.dbmate
 
@@ -23,7 +22,29 @@
           ExposedPorts = {
             "8501/tcp" = { };
           };
+          Labels = {
+            "org.opencontainers.image.description" =
+              "Nix binary cache proxy service -- with local caching and signing";
+            "org.opencontainers.image.licenses" = "MIT";
+            "org.opencontainers.image.source" = "https://github.com/kalbasit/ncps";
+            "org.opencontainers.image.title" = "ncps";
+            "org.opencontainers.image.url" = "https://github.com/kalbasit/ncps";
+          };
         };
       };
+
+      packages.push-docker-image = pkgs.writeShellScript "push-docker-image" ''
+        set -euo pipefail
+
+        if [[ ! -v DOCKER_IMAGE_TAGS ]]; then
+          echo "DOCKER_IMAGE_TAGS is not set but is required." >&2
+          exit 1
+        fi
+
+        for tag in $DOCKER_IMAGE_TAGS; do
+          ${pkgs.skopeo}/bin/skopeo --insecure-policy copy \
+            "docker-archive:${config.packages.docker}" docker://$tag
+        done
+      '';
     };
 }
