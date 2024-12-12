2008ae898c4c2425a361088b6af35db736934d99
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-12 14:46:44 -0800

    GitHub: Build and push docker image with Nix (#94)
    Use Nix to build the image and use skopeo to push it to Dockerhub; The
    action still uses some tools from Docker such as computing tags and
    logging in to Docker hub.

--- nix/packages/docker.nix
+++ nix/packages/docker.nix
@@ -6,19 +6,54 @@
       ...
     }:
     {
-      packages.docker = pkgs.dockerTools.buildImage {
-        name = "ncps";
-        tag = "latest";
-        copyToRoot = [
-          config.packages.ncps
+      packages.docker = pkgs.dockerTools.buildLayeredImage {
+        name = "kalbasit/ncps";
+        contents = [
+          # required for TLS certificate validation
+          pkgs.cacert
+
+          # required for working with timezones
+          pkgs.tzdata
+
+          # required for migrating the database
           pkgs.dbmate
+
+          # the ncps package
+          config.packages.ncps
         ];
         config = {
           Cmd = [ "/bin/ncps" ];
+          Env = [
+            "DBMATE_MIGRATIONS_DIR=/share/ncps/db/migrations"
+            "DBMATE_NO_DUMP_SCHEMA=true"
+          ];
           ExposedPorts = {
             "8501/tcp" = { };
           };
+          Labels = {
+            "org.opencontainers.image.description" =
+              "Nix binary cache proxy service -- with local caching and signing";
+            "org.opencontainers.image.licenses" = "MIT";
+            "org.opencontainers.image.source" = "https://github.com/kalbasit/ncps";
+            "org.opencontainers.image.title" = "ncps";
+            "org.opencontainers.image.url" = "https://github.com/kalbasit/ncps";
+          };
         };
       };
+
+      packages.push-docker-image = pkgs.writeShellScript "push-docker-image" ''
+        set -euo pipefail
+
+        if [[ ! -v DOCKER_IMAGE_TAGS ]]; then
+          echo "DOCKER_IMAGE_TAGS is not set but is required." >&2
+          exit 1
+        fi
+
+        for tag in $DOCKER_IMAGE_TAGS; do
+          echo "Pushing the image: $tag"
+          ${pkgs.skopeo}/bin/skopeo --insecure-policy copy \
+            "docker-archive:${config.packages.docker}" docker://$tag
+        done
+      '';
     };
 }
