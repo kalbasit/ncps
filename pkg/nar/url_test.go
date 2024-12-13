2c3455d0761735cd4712d8591f972895b438c26f
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-12 18:26:22 -0800

    fix the tests

--- pkg/nar/url_test.go
+++ pkg/nar/url_test.go
@@ -1,31 +1,31 @@
-//nolint:testpackage
-package helper
+package nar_test
 
 import (
 	"fmt"
 	"net/url"
 	"testing"
 
+	"github.com/kalbasit/ncps/pkg/nar"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestParseURL(t *testing.T) {
 	tests := []struct {
 		url    string
-		narURL URL
+		narURL nar.URL
 		err    error
 	}{
 		{
 			url: "",
-			err: ErrInvalidURL,
+			err: nar.ErrInvalidURL,
 		},
 		{
 			url: "helloworld",
-			err: ErrInvalidURL,
+			err: nar.ErrInvalidURL,
 		},
 		{
 			url: "nar/1mb5fxh7nzbx1b2q40bgzwjnjh8xqfap9mfnfqxlvvgvdyv8xwps.nar",
-			narURL: URL{
+			narURL: nar.URL{
 				Hash:        "1mb5fxh7nzbx1b2q40bgzwjnjh8xqfap9mfnfqxlvvgvdyv8xwps",
 				Compression: "",
 				Query:       url.Values{},
@@ -34,7 +34,7 @@ func TestParseURL(t *testing.T) {
 		},
 		{
 			url: "nar/1mb5fxh7nzbx1b2q40bgzwjnjh8xqfap9mfnfqxlvvgvdyv8xwps.nar.xz",
-			narURL: URL{
+			narURL: nar.URL{
 				Hash:        "1mb5fxh7nzbx1b2q40bgzwjnjh8xqfap9mfnfqxlvvgvdyv8xwps",
 				Compression: "xz",
 				Query:       url.Values{},
@@ -43,7 +43,7 @@ func TestParseURL(t *testing.T) {
 		},
 		{
 			url: "nar/1bn7c3bf5z32cdgylhbp9nzhh6ydib5ngsm6mdhsvf233g0nh1ac.nar?hash=1q8w6gl1ll0mwfkqc3c2yx005s6wwfrl",
-			narURL: URL{
+			narURL: nar.URL{
 				Hash:        "1bn7c3bf5z32cdgylhbp9nzhh6ydib5ngsm6mdhsvf233g0nh1ac",
 				Compression: "",
 				Query:       url.Values(map[string][]string{"hash": []string{"1q8w6gl1ll0mwfkqc3c2yx005s6wwfrl"}}),
@@ -52,7 +52,7 @@ func TestParseURL(t *testing.T) {
 		},
 		{
 			url: "nar/1bn7c3bf5z32cdgylhbp9nzhh6ydib5ngsm6mdhsvf233g0nh1ac.nar.xz?hash=1q8w6gl1ll0mwfkqc3c2yx005s6wwfrl",
-			narURL: URL{
+			narURL: nar.URL{
 				Hash:        "1bn7c3bf5z32cdgylhbp9nzhh6ydib5ngsm6mdhsvf233g0nh1ac",
 				Compression: "xz",
 				Query:       url.Values(map[string][]string{"hash": []string{"1q8w6gl1ll0mwfkqc3c2yx005s6wwfrl"}}),
@@ -67,7 +67,7 @@ func TestParseURL(t *testing.T) {
 		t.Run(fmt.Sprintf("ParseURL(%q)", test.url), func(t *testing.T) {
 			t.Parallel()
 
-			narURL, err := ParseURL(test.url)
+			narURL, err := nar.ParseURL(test.url)
 
 			if assert.ErrorIs(t, test.err, err) {
 				assert.Equal(t, test.narURL, narURL)
