ef7b6f207933e7bae959b7d23471b603e61a060b
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-14 14:01:24 -0800

    devshells no mor docker

--- nix/devshells/flake-module.nix
+++ nix/devshells/flake-module.nix
@@ -22,8 +22,6 @@
           ${config.pre-commit.installationScript}
 
           ${pkgs.gnused}/bin/sed -e "s:^\(go \)[0-9.]*$:\1''${_GO_VERSION}:" -i go.mod
-          ${pkgs.gnused}/bin/sed -e "s:^\(ARG GO_VERSION=\).*$:\1''${_GO_VERSION}:" -i Dockerfile
-          ${pkgs.gnused}/bin/sed -e "s:^\(ARG DBMATE_VERSION=\).*$:\1''${_DBMATE_VERSION}:" -i Dockerfile
         '';
       };
     };
