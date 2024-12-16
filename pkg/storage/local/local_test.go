a7e6df6e514bb93c817d62a325e727d7d72c1553
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-16 12:00:21 -0800

    failing test for new func

---
+++ pkg/storage/local/local_test.go
@@ -0,0 +1,154 @@
+package local_test
+
+import (
+	"context"
+	"io"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/kalbasit/ncps/pkg/storage/local"
+	"github.com/kalbasit/ncps/testhelper"
+)
+
+func TestNew(t *testing.T) {
+	t.Parallel()
+
+	t.Run("path is required", func(t *testing.T) {
+		t.Parallel()
+
+		_, err := local.New(newContext(), "")
+		assert.ErrorIs(t, err, local.ErrPathMustBeAbsolute)
+	})
+
+	t.Run("path is not absolute", func(t *testing.T) {
+		t.Parallel()
+
+		_, err := local.New(newContext(), "somedir")
+		assert.ErrorIs(t, err, local.ErrPathMustBeAbsolute)
+	})
+
+	t.Run("path must exist", func(t *testing.T) {
+		t.Parallel()
+
+		_, err := local.New(newContext(), "/non-existing")
+		assert.ErrorIs(t, err, local.ErrPathMustExist)
+	})
+
+	t.Run("path must be a directory", func(t *testing.T) {
+		t.Parallel()
+
+		f, err := os.CreateTemp("", "somefile")
+		require.NoError(t, err)
+		defer os.Remove(f.Name())
+
+		_, err = local.New(newContext(), f.Name())
+		assert.ErrorIs(t, err, local.ErrPathMustBeADirectory)
+	})
+
+	t.Run("path must be writable", func(t *testing.T) {
+		t.Parallel()
+
+		dir, err := os.MkdirTemp("", "cache-path-")
+		require.NoError(t, err)
+		defer os.RemoveAll(dir) // clean up
+
+		require.NoError(t, os.Chmod(dir, 0o500))
+
+		_, err = local.New(newContext(), dir)
+		assert.ErrorIs(t, err, local.ErrPathMustBeWritable)
+	})
+
+	t.Run("valid path must return no error", func(t *testing.T) {
+		t.Parallel()
+
+		_, err := local.New(newContext(), os.TempDir())
+		assert.NoError(t, err)
+	})
+
+	t.Run("should create directories", func(t *testing.T) {
+		t.Parallel()
+
+		dir, err := os.MkdirTemp("", "cache-path-")
+		require.NoError(t, err)
+		defer os.RemoveAll(dir) // clean up
+
+		dbFile := filepath.Join(dir, "var", "ncps", "db", "db.sqlite")
+		testhelper.CreateMigrateDatabase(t, dbFile)
+
+		_, err = local.New(newContext(), dir)
+		require.NoError(t, err)
+
+		dirs := []string{
+			"config",
+			"store",
+			filepath.Join("store", "narinfo"),
+			filepath.Join("store", "nar"),
+			filepath.Join("store", "tmp"),
+			filepath.Join("var", "ncps", "db"),
+		}
+
+		for _, p := range dirs {
+			t.Run("Checking that "+p+" exists", func(t *testing.T) {
+				assert.DirExists(t, filepath.Join(dir, p))
+			})
+		}
+	})
+
+	t.Run("store/tmp is removed on boot", func(t *testing.T) {
+		t.Parallel()
+
+		dir, err := os.MkdirTemp("", "cache-path-")
+		require.NoError(t, err)
+		defer os.RemoveAll(dir) // clean up
+
+		// create the directory tmp and add a file inside of it
+		err = os.MkdirAll(filepath.Join(dir, "store", "tmp"), 0o700)
+		require.NoError(t, err)
+
+		f, err := os.CreateTemp(filepath.Join(dir, "store", "tmp"), "hello")
+		require.NoError(t, err)
+
+		_, err = local.New(newContext(), dir)
+		require.NoError(t, err)
+
+		assert.NoFileExists(t, f.Name())
+	})
+}
+
+func TestGetSecretKey(t *testing.T) {
+}
+
+func TestPutSecretKey(t *testing.T) {
+}
+
+func TestDeleteSecretKey(t *testing.T) {
+}
+
+func TestGetNarInfo(t *testing.T) {
+}
+
+func TestPutNarInfo(t *testing.T) {
+}
+
+func TestDeleteNarInfo(t *testing.T) {
+}
+
+func TestGetNar(t *testing.T) {
+}
+
+func TestPutNar(t *testing.T) {
+}
+
+func TestDeleteNar(t *testing.T) {
+}
+
+func newContext() context.Context {
+	return zerolog.
+		New(io.Discard).
+		WithContext(context.Background())
+}
