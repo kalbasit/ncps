e7b3fd420a7db9cc2de5d3ce89afb01c2591ee76
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-16 12:09:13 -0800

    implement the store

--- pkg/storage/local/local.go
+++ pkg/storage/local/local.go
@@ -3,11 +3,31 @@ package local
 import (
 	"context"
 	"errors"
+	"fmt"
 	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
 
-	"github.com/kalbasit/ncps/pkg/nar"
 	"github.com/nix-community/go-nix/pkg/narinfo"
 	"github.com/nix-community/go-nix/pkg/narinfo/signature"
+	"github.com/rs/zerolog"
+
+	"github.com/kalbasit/ncps/pkg/nar"
+)
+
+var (
+	// ErrPathMustBeAbsolute is returned if the given path to New was not absolute.
+	ErrPathMustBeAbsolute = errors.New("path must be absolute")
+
+	// ErrPathMustExist is returned if the given path to New did not exist.
+	ErrPathMustExist = errors.New("path must exist")
+
+	// ErrPathMustBeADirectory is returned if the given path to New is not a directory.
+	ErrPathMustBeADirectory = errors.New("path must be a directory")
+
+	// ErrPathMustBeWritable is returned if the given path to New is not writable.
+	ErrPathMustBeWritable = errors.New("path must be writable")
 )
 
 // Store represents a local store and implements storage.Store.
@@ -15,8 +35,18 @@ type Store struct {
 	path string
 }
 
-func New(path string) (*Store, error) {
-	return nil, errors.New("not implemented")
+func New(ctx context.Context, path string) (*Store, error) {
+	if err := validatePath(ctx, path); err != nil {
+		return nil, err
+	}
+
+	s := &Store{path: path}
+
+	if err := s.setupDirs(); err != nil {
+		return nil, fmt.Errorf("error setting up the store directory: %w", err)
+	}
+
+	return s, nil
 }
 
 // GetSecretKey returns secret key from the store.
@@ -62,4 +92,80 @@ func (s *Store) PutNar(ctx context.Context, narURL nar.URL, body io.ReadCloser) 
 // DeleteNar deletes the nar from the store.
 func (s *Store) DeleteNar(ctx context.Context, narURL nar.URL) error {
 	return errors.New("not implemented")
+}
+
+func (s *Store) configPath() string       { return filepath.Join(s.path, "config") }
+func (s *Store) secretKeyPath() string    { return filepath.Join(s.configPath(), "cache.key") }
+func (s *Store) storePath() string        { return filepath.Join(s.path, "store") }
+func (s *Store) storeNarInfoPath() string { return filepath.Join(s.storePath(), "narinfo") }
+func (s *Store) storeNarPath() string     { return filepath.Join(s.storePath(), "nar") }
+func (s *Store) storeTMPPath() string     { return filepath.Join(s.storePath(), "tmp") }
+
+func (s *Store) setupDirs() error {
+	if err := os.RemoveAll(s.storeTMPPath()); err != nil {
+		return fmt.Errorf("error removing the temporary download directory: %w", err)
+	}
+
+	allPaths := []string{
+		s.configPath(),
+		s.storePath(),
+		s.storeNarInfoPath(),
+		s.storeNarPath(),
+		s.storeTMPPath(),
+	}
+
+	for _, p := range allPaths {
+		if err := os.MkdirAll(p, 0o700); err != nil {
+			return fmt.Errorf("error creating the directory %q: %w", p, err)
+		}
+	}
+
+	return nil
+}
+
+func validatePath(ctx context.Context, path string) error {
+	log := zerolog.Ctx(ctx)
+
+	if !filepath.IsAbs(path) {
+		log.Error().Str("path", path).Msg("path is not absolute")
+
+		return ErrPathMustBeAbsolute
+	}
+
+	info, err := os.Stat(path)
+	if errors.Is(err, fs.ErrNotExist) {
+		log.Error().Str("path", path).Msg("path does not exist")
+
+		return ErrPathMustExist
+	}
+
+	if !info.IsDir() {
+		log.Error().Str("path", path).Msg("path is not a directory")
+
+		return ErrPathMustBeADirectory
+	}
+
+	if !isWritable(ctx, path) {
+		return ErrPathMustBeWritable
+	}
+
+	return nil
+}
+
+func isWritable(ctx context.Context, path string) bool {
+	log := zerolog.Ctx(ctx)
+	tmpFile, err := os.CreateTemp(path, "write_test")
+	if err != nil {
+		log.Error().
+			Err(err).
+			Str("path", path).
+			Msg("error writing a temp file in the path")
+
+		return false
+	}
+
+	defer os.Remove(tmpFile.Name())
+	defer tmpFile.Close()
+
+	return true
 }
