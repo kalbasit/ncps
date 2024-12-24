124307563922bae62422c9a56a7950e871691ec9
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-23 20:40:32 -0800

    properly stop otelwriter

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"net/url"
 	"os"
 	"time"
 
@@ -20,11 +21,48 @@ import (
 var Version = "dev"
 
 func New() *cli.Command {
+	var otelWriter *otelzerolog.OtelWriter
+
 	return &cli.Command{
 		Name:    "ncps",
 		Usage:   "Nix Binary Cache Proxy Service",
 		Version: Version,
-		Before:  beforeFunc,
+		After: func(ctx context.Context, _ *cli.Command) error {
+			if otelWriter != nil {
+				return otelWriter.Close(ctx)
+			}
+
+			return nil
+		},
+		Before: func(ctx context.Context, cmd *cli.Command) (context.Context, error) {
+			logLvl := cmd.String("log-level")
+
+			lvl, err := zerolog.ParseLevel(logLvl)
+			if err != nil {
+				return ctx, fmt.Errorf("error parsing the log-level %q: %w", logLvl, err)
+			}
+
+			var output io.Writer = os.Stdout
+
+			if colURL := cmd.String("log-otel-grpc-endpoint"); colURL != "" {
+				otelWriter, err = otelzerolog.NewOtelWriter(ctx, colURL, "ncps")
+				if err != nil {
+					return ctx, err
+				}
+
+				output = zerolog.MultiLevelWriter(os.Stdout, otelWriter)
+			}
+
+			if term.IsTerminal(int(os.Stdout.Fd())) {
+				output = zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+			}
+
+			log := zerolog.New(output).Level(lvl)
+
+			log.Info().Str("log-level", lvl.String()).Msg("logger created")
+
+			return log.WithContext(ctx), nil
+		},
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:    "log-level",
@@ -42,8 +80,8 @@ func New() *cli.Command {
 				Usage:   "Forward logs to an OpenTelemetry gRPC endpoint",
 				Sources: cli.EnvVars("LOG_OTEL_GRPC_ENDPOINT"),
 				Value:   "",
-				Validator: func(lvl string) error {
-					_, err := zerolog.ParseLevel(lvl)
+				Validator: func(colURL string) error {
+					_, err := url.Parse(colURL)
 
 					return err
 				},
@@ -53,31 +91,4 @@ func New() *cli.Command {
 			serveCommand(),
 		},
 	}
-}
-
-func beforeFunc(ctx context.Context, cmd *cli.Command) (context.Context, error) {
-	logLvl := cmd.String("log-level")
-
-	lvl, err := zerolog.ParseLevel(logLvl)
-	if err != nil {
-		return ctx, fmt.Errorf("error parsing the log-level %q: %w", logLvl, err)
-	}
-
-	// Create the OpenTelemetry writer
-	otelWriter, err := otelzerolog.NewOtelWriter(ctx, "localhost:14317", "ncps")
-	if err != nil {
-		return ctx, err
-	}
-
-	var output io.Writer = zerolog.MultiLevelWriter(os.Stdout, otelWriter)
-
-	if term.IsTerminal(int(os.Stdout.Fd())) {
-		output = zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
-	}
-
-	log := zerolog.New(output).Level(lvl)
-
-	log.Info().Str("log-level", lvl.String()).Msg("logger created")
-
-	return log.WithContext(ctx), nil
 }
