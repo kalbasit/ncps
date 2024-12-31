e2f1369cf571c5f9eccf98e65a6a54511e5ce5bf
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-31 11:34:10 -0800

    add testdata/testhelper to checks

--- nix/checks/flake-module.nix
+++ nix/checks/flake-module.nix
@@ -16,6 +16,16 @@
         packages
         // devShells
         // {
+          package-ncps = packages."package-ncps".overrideAttrs (oa: {
+            # The testdata and testhelper packages are not used by the main
+            # package but do (could) have tests. Have the check include them to
+            # ensure they pass should they have tests.
+            subPackages = oa.subPackages ++ [
+              "testdata"
+              "testhelper"
+            ];
+          });
+
           golangci-lint = config.packages.ncps.overrideAttrs (oa: {
             name = "golangci-lint";
             src = ../../.;
