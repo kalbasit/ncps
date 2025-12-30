7502507c637d7c2c4ea7948393d09a2392a2651f
Wael Nasreddine <wael.nasreddine@gmail.com>
2025-12-29 22:38:15 -0800

    fix them here

--- db/migrations/mysql/20241210054814_create-narinfos-table.sql
+++ db/migrations/mysql/20241210054814_create-narinfos-table.sql
@@ -5,11 +5,11 @@ CREATE TABLE narinfos (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NULL DEFAULT NULL,
     last_accessed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE KEY idx_narinfos_hash (hash(255))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    UNIQUE KEY idx_narinfos_hash (hash (255))
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 CREATE INDEX idx_narinfos_last_accessed_at ON narinfos (last_accessed_at);
 
 -- migrate:down
-DROP INDEX IF EXISTS idx_narinfos_last_accessed_at ON narinfos;
+DROP INDEX idx_narinfos_last_accessed_at ON narinfos;
 DROP TABLE IF EXISTS narinfos;
--- db/migrations/mysql/20241210054829_create-nars-table.sql
+++ db/migrations/mysql/20241210054829_create-nars-table.sql
@@ -8,11 +8,11 @@ CREATE TABLE nars (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NULL DEFAULT NULL,
     last_accessed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE KEY idx_nars_hash (hash(255)),
+    UNIQUE KEY idx_nars_hash (hash (255)),
     KEY idx_nars_narinfo_id (narinfo_id),
     KEY idx_nars_last_accessed_at (last_accessed_at),
     CONSTRAINT fk_nars_narinfo FOREIGN KEY (narinfo_id) REFERENCES narinfos (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 -- migrate:down
 DROP TABLE IF EXISTS nars;
