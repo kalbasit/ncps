b4c3249a8b59b270405e38e372860086832fa997
Wael Nasreddine <wael.nasreddine@gmail.com>
2026-01-04 19:08:08 -0800

    remove indexes that were removed now

--- db/migrations/sqlite/20260105030513_support_shared_nars_between_narinfos.sql
+++ db/migrations/sqlite/20260105030513_support_shared_nars_between_narinfos.sql
@@ -55,8 +55,6 @@ SELECT narinfo_id, id
 FROM nars;
 
 -- Step 5: Drop old nars table and its indexes
-DROP INDEX idx_nars_id;
-DROP INDEX idx_nars_hash;
 DROP INDEX idx_nars_narinfo_id;
 DROP INDEX idx_nars_last_accessed_at;
 DROP TABLE nars;
