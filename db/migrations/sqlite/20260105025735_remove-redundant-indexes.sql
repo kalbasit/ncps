10417c27231ab055e411b5a28f9a70cf5e4bd935
Wael Nasreddine <wael.nasreddine@gmail.com>
2026-01-04 19:03:11 -0800

    add if not exists

--- db/migrations/sqlite/20260105025735_remove-redundant-indexes.sql
+++ db/migrations/sqlite/20260105025735_remove-redundant-indexes.sql
@@ -5,7 +5,7 @@ DROP INDEX IF EXISTS idx_nars_id;
 DROP INDEX IF EXISTS idx_nars_hash;
 
 -- migrate:down
-CREATE UNIQUE INDEX idx_narinfos_id ON narinfos (id);
-CREATE UNIQUE INDEX idx_narinfos_hash ON narinfos (hash);
-CREATE UNIQUE INDEX idx_nars_id ON nars (id);
-CREATE UNIQUE INDEX idx_nars_hash ON nars (hash);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_narinfos_id ON narinfos (id);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_narinfos_hash ON narinfos (hash);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_nars_id ON nars (id);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_nars_hash ON nars (hash);
