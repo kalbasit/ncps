36638bb9f0ca2069c4dcdaf71d8ac8f4d4293282
Wael Nasreddine <wael.nasreddine@gmail.com>
2026-01-07 01:11:25 -0800

    run nix fmt

--- pkg/analytics/analytics.go
+++ pkg/analytics/analytics.go
@@ -40,7 +40,7 @@ func Start(
 
 	// Create a dedicated OTLP Exporter
 	// Uncomment the line below to see the metrics on stdout.
-	//exporter, err := stdoutmetric.New()
+	// exporter, err := stdoutmetric.New()
 	exporter, err := otlpmetrichttp.New(ctx,
 		otlpmetrichttp.WithEndpointURL(DefaultEndpointURL),
 		otlpmetrichttp.WithCompression(otlpmetrichttp.GzipCompression),
