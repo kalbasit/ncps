ccbd292fdea348337a500bc836d3b7c2ada92d10
Wael Nasreddine <wael.nasreddine@gmail.com>
2026-01-10 01:19:06 -0800

    remove dead code

--- pkg/lock/mysql/export_test.go
+++ pkg/lock/mysql/export_test.go
@@ -8,16 +8,6 @@ import (
 	"github.com/kalbasit/ncps/pkg/lock"
 )
 
-// Export internal identifiers for testing.
-// func NewCircuitBreaker(threshold int, timeout time.Duration) *circuitBreaker {
-// 	return newCircuitBreaker(threshold, timeout)
-// }
-
-// IsOpen returns true if the circuit breaker is open.
-// func (cb *circuitBreaker) IsOpen() bool   { return !cb.AllowRequest() }
-// func (cb *circuitBreaker) RecordFailure() { cb.recordFailure() }
-// func (cb *circuitBreaker) RecordSuccess() { cb.recordSuccess() }
-
 func CalculateBackoff(cfg lock.RetryConfig, attempt int) time.Duration {
 	return calculateBackoff(cfg, attempt)
 }
