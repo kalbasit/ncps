1422acd81a1362a9f916e33c7fc9f8d63fd00d5e
Wael Nasreddine <wael.nasreddine@gmail.com>
2026-01-18 19:35:09 -0800

    improve test func

--- nix/gen-db-wrappers/src/main_test.go
+++ nix/gen-db-wrappers/src/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"go/ast"
 	"strings"
 	"testing"
@@ -239,9 +240,16 @@ func TestWrapperTemplate(t *testing.T) {
 		"toSingular":          toSingular,
 		"trimPrefix":          strings.TrimPrefix,
 		"dict": func(values ...interface{}) (map[string]interface{}, error) {
+			if len(values)%2 != 0 {
+				return nil, fmt.Errorf("invalid dict call")
+			}
 			dict := make(map[string]interface{}, len(values)/2)
 			for i := 0; i < len(values); i += 2 {
-				dict[values[i].(string)] = values[i+1]
+				key, ok := values[i].(string)
+				if !ok {
+					return nil, fmt.Errorf("dict keys must be strings")
+				}
+				dict[key] = values[i+1]
 			}
 			return dict, nil
 		},
