dda68079e6598d34289c0772f31d2a3fec9ec891
Wael Nasreddine <wael.nasreddine@gmail.com>
2026-01-26 15:41:17 -0800

    refactor: simplify golangci-lint check in nix/checks
    This simplifies the golangci-lint check by using pkgs.stdenvNoCC.mkDerivation
    instead of overriding config.packages.ncps. This is more efficient as it
    avoids unnecessary build steps from buildGoModule. It also updates HOME
    to $NIX_BUILD_TOP for consistency in the build environment.

--- nix/checks/flake-module.nix
+++ nix/checks/flake-module.nix
@@ -3,7 +3,6 @@
     {
       self',
       pkgs,
-      config,
       ...
     }:
     {
@@ -11,23 +10,18 @@
         self'.packages
         // self'.devShells
         // {
-          # TODO: Simplify this to not use buildGoModule as it seems to be a
-          # waste of time. This could be a simple stdenvNoCC.mkDerviation.
-          golangci-lint-check = config.packages.ncps.overrideAttrs (oa: {
+          golangci-lint-check = pkgs.stdenvNoCC.mkDerivation {
             name = "golangci-lint-check";
             src = ../../.;
-            # ensure the output is only out since it's the only thing this package does.
-            outputs = [ "out" ];
-            nativeBuildInputs = oa.nativeBuildInputs ++ [ pkgs.golangci-lint ];
+            nativeBuildInputs = [ pkgs.golangci-lint ];
             buildPhase = ''
-              HOME=$TMPDIR
+              HOME=$NIX_BUILD_TOP
               golangci-lint run --timeout 10m
             '';
             installPhase = ''
               touch $out
             '';
-            doCheck = false;
-          });
+          };
         };
     };
 }
