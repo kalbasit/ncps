00a3515de7cbae91f875e9d3dc5bf168dd574f6c
Wael Nasreddine <wael.nasreddine@gmail.com>
2026-02-03 14:25:10 -0800

    use regex

--- pkg/helper/url_path.go
+++ pkg/helper/url_path.go
@@ -1,19 +1,13 @@
 package helper
 
+import "regexp"
+
+var isValidHashRegexp = regexp.MustCompile(`^([a-z0-9]{32}|[a-z0-9]{52})$`)
+
 // NarInfoURLPath returns the path of the narinfo file given a hash.
 func NarInfoURLPath(hash string) string { return "/" + hash + ".narinfo" }
 
 // IsValidHash returns true if the hash is valid (32 or 52 lowercase alphanumeric characters).
 func IsValidHash(hash string) bool {
-	if len(hash) != 32 && len(hash) != 52 {
-		return false
-	}
-
-	for _, r := range hash {
-		if (r < 'a' || r > 'z') && (r < '0' || r > '9') {
-			return false
-		}
-	}
-
-	return true
+	return isValidHashRegexp.MatchString(hash)
 }
