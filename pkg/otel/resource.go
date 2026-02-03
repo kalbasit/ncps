cc8827a4521c4f6333883c7116be09fc23629127
Wael Nasreddine <wael.nasreddine@gmail.com>
2026-02-02 20:29:22 -0800

    update the semconv version

--- pkg/otel/resource.go
+++ pkg/otel/resource.go
@@ -6,7 +6,7 @@ import (
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/sdk/resource"
 
-	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.39.0"
 )
 
 // NewResource creates a new OpenTelemetry resource with standard attributes.
