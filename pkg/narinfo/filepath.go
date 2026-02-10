3e62008f8d474d63aa7a2430a035aac7d2b38d39
Wael Nasreddine <wael.nasreddine@gmail.com>
2026-02-09 21:47:11 -0800

    refactor: improve pkg/narinfo consistency with pkg/nar

--- pkg/narinfo/filepath.go
+++ pkg/narinfo/filepath.go
@@ -1,29 +1,10 @@
 package narinfo
 
 import (
-	"errors"
 	"fmt"
-	"regexp"
 
 	"github.com/kalbasit/ncps/pkg/helper"
 )
-
-var (
-	// ErrInvalidHash is returned if the hash is invalid.
-	ErrInvalidHash = errors.New("invalid narinfo hash")
-
-	// hashRegexp is used to validate hashes.
-	hashRegexp = regexp.MustCompile(`^[a-z0-9]+$`)
-)
-
-// ValidateHash validates the given hash.
-func ValidateHash(hash string) error {
-	if !hashRegexp.MatchString(hash) {
-		return ErrInvalidHash
-	}
-
-	return nil
-}
 
 // FilePath returns the path of the narinfo file given a hash.
 func FilePath(hash string) (string, error) {
