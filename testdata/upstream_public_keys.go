3f8d8c138bf625393ecda57d7822f81875accb9d
Wael Nasreddine <wael.nasreddine@gmail.com>
2026-02-09 22:01:12 -0800

    fix: Add support for missing FileSize and FileHash (#823)
    This change allows ncps to work correctly when FileSize and/or FileHash
    are missing. It computes the FileSize if it's missing and Compression is
    none but returns an error for invalid narinfo otherwise. FileHash is not
    computed at this time.
    
    part of fixing #806
    
    (cherry picked from commit b13ebb2fd70b995f7789fcd835dd27fa20adbaf9)

--- testdata/upstream_public_keys.go
+++ testdata/upstream_public_keys.go
@@ -1,5 +1,7 @@
 package testdata
 
 func PublicKeys() []string {
-	return []string{"cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="}
+	return []string{
+		"cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=",
+	}
 }
