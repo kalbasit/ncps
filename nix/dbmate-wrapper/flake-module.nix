9d2d2eb64782015edcda042cd7c37d6b794744ef
Wael Nasreddine <wael.nasreddine@gmail.com>
2026-02-21 08:35:15 -0800

    feat: simplify the dbmate-wrapper package (#949)
    The dbmate-wrapper package is expecting a dbmate.real in its path and
    relying on a different setup in the Docker image and in the devShell.
    Instead of these assumptions, wrap the program and set an environment
    variable, DBMATE_BIN, used by the wrapper to find the binary it needs.

--- nix/dbmate-wrapper/flake-module.nix
+++ nix/dbmate-wrapper/flake-module.nix
@@ -1,12 +1,20 @@
 {
   perSystem =
-    { pkgs, ... }:
+    { lib, pkgs, ... }:
     {
       packages.dbmate-wrapper = pkgs.buildGoModule {
         name = "dbmate-wrapper";
         src = ./src;
 
         vendorHash = null;
+
+        buildInputs = lib.singleton pkgs.dbmate;
+        nativeBuildInputs = lib.singleton pkgs.makeBinaryWrapper;
+
+        postInstall = ''
+          # the dbmate-wrapper needs access to the original dbmate executable, wrap it so it can find it correctly.
+          wrapProgram $out/bin/dbmate-wrapper --set DBMATE_BIN ${lib.getExe pkgs.dbmate}
+        '';
 
         meta = {
           description = "Wrapper for dbmate that auto-detects migrations directory from database URL";
