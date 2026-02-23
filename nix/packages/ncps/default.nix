889866e1edf7f42a61759f6efe13dc671a42f3b5
kalbasit <87115+kalbasit@users.noreply.github.com>
2026-02-23 03:52:55 +0000

    Auto Update Vendor Hash for ncps

--- nix/packages/ncps/default.nix
+++ nix/packages/ncps/default.nix
@@ -16,7 +16,7 @@
             in
             if tag != "" then tag else rev;
 
-          vendorHash = "sha256-QZikr0kE/kvnI4RG02lxVpG4teTg3Uo68st9xLlbfm0=";
+          vendorHash = "sha256-elg7cYjW4YsP4IqQ2qiMHbUlIKKKNOo7T4TLHIsYDuA=";
         in
         pkgs.buildGoModule {
           inherit version vendorHash;
